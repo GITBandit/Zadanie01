f5f96a43070662c5ffc517ece9b40350420777c6
Dominik Jakubowski <dominikjakubowski@gmail.com>
2017-11-23 22:11:26 +0100

    V1.1 Tested version

--- src/Zad01.java
+++ src/Zad01.java
@@ -2,7 +2,9 @@ public class Zad01 {
 
     public static void main(String[] args) {
 
-
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
 
 
 
